ac38207c3aecad50a725b709a6d85c0304d71c73
Alex <situalex123@gmail.com>
2025-10-03 15:06:56 -0700

    bookmark

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,13 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Button, Pressable } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 import { icons } from '@/constants/icons'
+import { bookmarkMovie } from '@/services/supabase'
 
-const MovieCard = ({ id, poster_path, title, vote_average, release_date, cardWidth }: Movie & {cardWidth?: number}) => {
+const MovieCard = ({ id, poster_path, title, vote_average, release_date, cardWidth }: Movie & { cardWidth?: number }) => {
     return (
         <Link href={`/movies/${id}`} asChild>
-            <TouchableOpacity className='w-[28%] mx-1' style={cardWidth ?{width:cardWidth}: undefined}>
+            <TouchableOpacity className='w-[28%] mx-1' style={cardWidth ? { width: cardWidth } : undefined}>
                 <Image source={{
                     uri: poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
 
@@ -19,12 +20,13 @@ const MovieCard = ({ id, poster_path, title, vote_average, release_date, cardWid
 
                 <View className='flex-row items-center justify-start gap-x-1'>
                     <Image source={icons.star} className='size-4'></Image>
-                    <Text className='text-white text-xs font-bold uppercase'>{vote_average.toFixed(1) }</Text>
+                    <Text className='text-white text-xs font-bold uppercase'>{vote_average.toFixed(1)}</Text>
+
                 </View>
                 <View className='flex-row itmes-center justify-between'>
                     <Text className='text-xs text-neutral-300 font-medium mt-1'>
                         {release_date?.split('-')[0]}
-                        
+
                     </Text>
                     {/* In case we add tv shows later*/}
                     {/* <Text className='text-xs font-medium text-light-300 uppercase'>Movie</Text> */}
--- services/supabase.ts
+++ services/supabase.ts
@@ -31,4 +31,44 @@ export const getSupabase = () => {
     return supabase;
 }
 
+async function getCurrentUser() {
+    try {
+        const { data: { user }, error } = await supabase.auth.getUser()
+
+        if (error) {
+            console.error('Error getting user:', error.message)
+            return null
+        }
+        if (user) {
+            console.log('Current User ID:', user.id)
+            return user
+        } else {
+            console.log('No user currently logged in.')
+            return null
+        }
+    } catch (error) {
+        console.error('An unexpected error occurred')
+        return null
+    }
+}
+
+export async function bookmarkMovie(movie_id: string) {
+    const user = await getCurrentUser()
+
+    if (!user) {
+        throw new Error("Error getting user")
+    }
+    const userId = user?.id
+
+    const data = {
+        user_id: userId ,
+        movie_id: movie_id 
+    }
+
+    const { data: bookmarkData, error } = await supabase.from('watched_movies').insert([data]).select()
+    if (error) {
+        throw new Error(`Failed to bookmark movie: ${error.message}`)
+    }
+}
+
 export const supabase = getSupabase();
